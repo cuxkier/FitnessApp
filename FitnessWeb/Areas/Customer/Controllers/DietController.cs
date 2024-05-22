f42729f2c6f7433b3960a9b5e33f4ca412b6478c
Adrianna 126341 <pzx103969@student.wsb.poznan.pl>
2024-05-23 00:39:16 +0200

    comment

---
+++ FitnessWeb/Areas/Customer/Controllers/DietController.cs
@@ -0,0 +1,74 @@
+﻿using Fitness.DataAccess.Repository.IRepository;
+using Fitness.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace Fitness.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class DietController : Controller
+    {
+        private readonly ILogger<DietController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DietController(ILogger<DietController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Diet> dietList = _unitOfWork.Diet.GetAll(includeProperties: "DietsCategory");
+            return View(dietList);
+        }
+
+        public IActionResult Details(int dietId)
+        {
+            ShoppingCart cart = new()
+            {
+                Diet = _unitOfWork.Diet.Get(u => u.Id == dietId, includeProperties: "DietsCategory"),
+                Count = 1,
+                DietId = dietId
+            };
+            return View(cart);
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId &&
+            u.DietId == shoppingCart.DietId);
+
+            if (cartFromDb != null)
+            {
+                //koszyk istnieje
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                //dodaj koszyk
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+
+            TempData["success"] = "Koszyk został zaktualizowany.";
+            _unitOfWork.Save();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
