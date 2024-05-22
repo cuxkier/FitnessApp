3a3df80563683ba0f25687a9728d1e83cb0203d9
nazwa <bla@bla.pl>
2024-05-22 23:29:45 +0200

    fitnesiarkaa

---
+++ FitnessWeb/Areas/Customer/Controllers/TrainersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class TrainersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
